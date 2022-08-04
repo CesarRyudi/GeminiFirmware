8b69e49e630cc3f470869544e8231567b07f67d1
CesarRyudi <106624143+CesarRyudi@users.noreply.github.com>
2022-08-04 09:23:28 -0300

    xx

--- Gemini/Gemini.ino
+++ Gemini/Gemini.ino
@@ -1,15 +1,11 @@
 #include "Gemini_begin.h""
 
 void loop(){
-    //gemini.dormir(3);
-    debugDelay(1000);
+    gemini.dormir(3);
     if(gemini.horaDeAcordar()) {
         debugln("Hora de acordar");
         gemini.acordar();
     }
 
-    if(Serial.available()){
-        memoria.read();
-    }
 }
 
